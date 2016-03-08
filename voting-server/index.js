f4d5886dcf8473ac7e95cf6d0b6bf1e4142e882a
Vivek Ramaswamy <vivram@microsoft.com>
2016-03-07 17:42:51 -0800

    Initial Commit

---
+++ voting-server/index.js
@@ -0,0 +1,11 @@
+import makeStore from './store';
+import startServer from './server';
+
+export const store = makeStore();
+startServer(store);
+
+store.dispatch({
+  type: 'SET_ENTRIES',
+  entries: require('./entries.json')
+});
+store.dispatch({type: 'NEXT'});
